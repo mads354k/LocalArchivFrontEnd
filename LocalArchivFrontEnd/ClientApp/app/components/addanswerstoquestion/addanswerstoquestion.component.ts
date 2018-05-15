122c7a67c9a30228636571a8b71cd94887ccc176
Mads Peter Østergaard <momfy@hotmail.dk>
2018-05-15 12:49:32 +0200

    added selectors

---
+++ LocalArchivFrontEnd/ClientApp/app/components/addanswerstoquestion/addanswerstoquestion.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, Inject, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Answer } from '../../answer/answer.model';
+import { Question } from '../../question/question.model';
+
+@Component({
+    selector: 'addanswerstoquestion',
+    templateUrl: './addanswerstoquestion.component.html'
+})
+export class AnswerToQuestionComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
---
+++ LocalArchivFrontEnd/ClientApp/app/components/addquestiontogame/addquestiontogame.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, Inject, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Game } from '../../game/game.model';
+import { Question } from '../../question/question.model';
+
+@Component({
+    selector: 'addquestiontogame',
+    templateUrl: './addquestiontogame.component.html'
+})
+export class QuestionToGameComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
