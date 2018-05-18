c776f4e044d7b7a50450646dc9d5d6ac6cffcb71
Mads Peter Østergaard <momfy@hotmail.dk>
2018-05-18 12:14:58 +0200

    implemented creation of questions and answers, files not working

--- LocalArchivFrontEnd/ClientApp/app/components/createquestion/createquestion.component.ts
+++ LocalArchivFrontEnd/ClientApp/app/components/createquestion/createquestion.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { Question } from '../../question/question.model';
+import { Answer } from '../../answer/answer.model';
+import { GameQuestion } from '../../gamequestion/gamequestion.model';
+import { RoundQuestion } from '../../roundquestion/roundquestion.model';
 
 @Component({
     selector: 'createquestion',
@@ -9,26 +12,85 @@ import { Question } from '../../question/question.model';
 })
 export class CreateQuestionComponent implements OnInit{
     private http: Http;
-    questionsCreated: number = 0;
+    questionsCreated: number;
 
     constructor(http: Http) { this.http = http; }
 
     ngOnInit() {
+        this.http.get('http://localhost:3000/gamequestions').subscribe(result => {
+            var gameQuestions = result.json() as GameQuestion[];
+            this.questionsCreated = 0;
+            for (let gameQuestion of gameQuestions) {
+                if (gameQuestion.gameId == Number(localStorage.getItem('ActiveGame'))) {
+                    this.questionsCreated += 1;
+                }
+            }
+        }, error => console.log(error));
     }
 
     cancelCreation() {
-
+        window.location.href = 'home';
     }
 
-    onSubmit(form: any) {
-        var question = new Question(0, form.description, form.questionType, form.picture, form.hint);
-        this.http.post('http://localhost:3000/questions', question).subscribe(result => {
-            var question = result.json() as Question;
-            if (localStorage.getItem('ActiveQuestion') != null) {
-                localStorage.removeItem('ActiveQuestion');
-            }
-            localStorage.setItem('ActiveQuestion', question.questionId + '');
-            alert('Question created and selected');
-        }, error => console.error(error));
+    addToGame() {
+        var description = <HTMLInputElement>document.getElementById('questionB');
+        var picture = <HTMLInputElement>document.getElementById('questionP');
+        var hint = <HTMLInputElement>document.getElementById('questionH');
+
+        if (description != null && picture != null && hint != null) {
+            var file = picture.files[0];
+            var question = new Question(0, description.value, 'Text', file, hint.value);
+            this.http.post('http://localhost:3000/questions', question).subscribe(result => {
+                var question = result.json() as Question;
+                if (localStorage.getItem('ActiveQuestion') != null) {
+                    localStorage.removeItem('ActiveQuestion');
+                }
+                localStorage.setItem('ActiveQuestion', question.questionId + '');
+
+                // var inputT = <HTMLInputElement>document.getElementById('answerT');
+                var inputB1 = <HTMLInputElement>document.getElementById('answerB1');
+                var inputP1 = <HTMLInputElement>document.getElementById('answerP1');
+                var inputC1 = <HTMLInputElement>document.getElementById('answerC1');
+
+                this.createAndAddAnswer(inputB1, inputP1, inputC1);
+
+                var inputB2 = <HTMLInputElement>document.getElementById('answerB2');
+                var inputP2 = <HTMLInputElement>document.getElementById('answerP2');
+                var inputC2 = <HTMLInputElement>document.getElementById('answerC2');
+
+                this.createAndAddAnswer(inputB2, inputP2, inputC2);
+
+                var inputB3 = <HTMLInputElement>document.getElementById('answerB3');
+                var inputP3 = <HTMLInputElement>document.getElementById('answerP3');
+                var inputC3 = <HTMLInputElement>document.getElementById('answerC3');
+
+                this.createAndAddAnswer(inputB3, inputP3, inputC3);
+
+                var inputB4 = <HTMLInputElement>document.getElementById('answerB4');
+                var inputP4 = <HTMLInputElement>document.getElementById('answerP4');
+                var inputC4 = <HTMLInputElement>document.getElementById('answerC4');
+
+                this.createAndAddAnswer(inputB4, inputP4, inputC4);
+
+                var gameQuestion = new GameQuestion(0, Number(localStorage.getItem('ActiveGame')), Number(localStorage.getItem('ActiveQuestion')));
+                this.http.post('http://localhost:3000/gamequestions', gameQuestion).subscribe(result => {
+                    
+                }, error => console.log(error));
+            }, error => console.error(error));
+        }      
+    }
+
+    createAndAddAnswer(inputB: HTMLInputElement, inputP: HTMLInputElement, inputC: HTMLInputElement) {
+        if (inputB != null && inputC != null && inputP != null) {
+            var file = inputP.files[0];
+            var answer = new Answer(0, inputB.value, 'Text', file);
+            this.http.post('http://localhost:3000/answers', answer).subscribe(result => {
+                var answer = result.json() as Answer;
+                var roundQuestion = new RoundQuestion(0, Number(localStorage.getItem('ActiveQuestion')), answer.answerId, Boolean(inputC.value));
+                this.http.post('http://localhost:3000/roundquestions', roundQuestion).subscribe(result => {
+
+                }, error => console.log(error));
+            }, error => console.error(error));
+        }
     }
 }
--- LocalArchivFrontEnd/ClientApp/app/components/home/home.component.ts
+++ LocalArchivFrontEnd/ClientApp/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Game } from '../../game/game.model';
 
 @Component({
     selector: 'home',
@@ -6,7 +8,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    constructor(private http: Http) {}
+
     nextPage() {
-        window.location.href = 'createquestion';
+        var name = <HTMLInputElement> document.getElementById('name');
+        var date = <HTMLInputElement> document.getElementById('date');
+        if (name != null && date != null) {
+            var game = new Game(0, name.value, Number(date.value));
+            this.http.post('http://localhost:3000/games', game).subscribe(result => {
+                var game = result.json() as Game;
+                if (localStorage.getItem('ActiveGame') != null) {
+                    localStorage.removeItem('ActiveGame');
+                }
+                localStorage.setItem('ActiveGame', game.gameId + '');
+                alert('Game created and selected');
+            }, error => console.error(error)); 
+            window.location.href = 'createquestion';
+        }
     }
+
+
 }
