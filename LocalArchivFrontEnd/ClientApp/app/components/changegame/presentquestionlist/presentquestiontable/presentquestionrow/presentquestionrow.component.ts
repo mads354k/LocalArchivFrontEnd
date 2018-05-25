7f7ca159c0c27b67135ef440decbf3771bc33d5a
Mads Peter Østergaard <momfy@hotmail.dk>
2018-05-25 09:30:17 +0200

    remove question from game from list

--- LocalArchivFrontEnd/ClientApp/app/components/changegame/presentquestionlist/presentquestiontable/presentquestionrow/presentquestionrow.component.ts
+++ LocalArchivFrontEnd/ClientApp/app/components/changegame/presentquestionlist/presentquestiontable/presentquestionrow/presentquestionrow.component.ts
@@ -11,7 +11,21 @@ export class PresentQuestionRowComponent implements OnInit {
     @Input() question: Question;
 
     updateRecord() {
+        var gameId = Number(localStorage.getItem('ChangeGame'));
 
+        this.http.get('http://localhost:3000/gamequestions').subscribe(result => {
+            var gameQuestions = result.json() as GameQuestion[];
+            var gameQuestionId;
+            for (let gameQuestion of gameQuestions) {
+                if (gameQuestion.gameId == gameId && gameQuestion.questionId == this.question.questionId) {
+                    gameQuestionId = gameQuestion.id;
+                    break;
+                }
+            }
+            this.http.delete('http://localhost:3000/gamequestions/' + gameQuestionId).subscribe(result => {
+                window.location.reload();
+            }, error => console.log(error));
+        }, error => console.log(error));
     }
 
     constructor(private http: Http) { }
