8b7bd72e3f7fb55de226d33f83f673b43c19be8e
Mads Peter Østergaard <momfy@hotmail.dk>
2018-05-25 09:58:13 +0200

    added all answers list

---
+++ LocalArchivFrontEnd/ClientApp/app/components/changequestion/avalibleanswerlist/avalibleanswerlist.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Question } from '../../../question/question.model';
+
+@Component({
+    selector: 'avaliblequestionlist',
+    templateUrl: './avaliblequestionlist.component.html',
+})
+export class AvalibleQuestionListComponent {
+    questions: Question[];
+
+    constructor(private http: Http) { }
+
+    ngOnInit() {
+        this.http.get('http://localhost:3000/questions').subscribe(result => {
+            this.questions = result.json() as Question[];
+        }, error => console.log(error));
+    }
+}
---
+++ LocalArchivFrontEnd/ClientApp/app/components/changequestion/avalibleanswerlist/avalibleanswertable/avalibleanswerrow/avalibleanswerrow.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, Inject, OnInit, Input } from '@angular/core';
+import { Http } from '@angular/http';
+import { Question } from '../../../../../question/question.model';
+import { GameQuestion } from '../../../../../gamequestion/gamequestion.model';
+
+@Component({
+    selector: '[avaliblequestionrow]',
+    templateUrl: './avaliblequestionrow.component.html'
+})
+export class AvalibleQuestionRowComponent implements OnInit {
+    @Input() question: Question;
+
+    updateRecord() {
+        var gameId = Number(localStorage.getItem('ChangeGame'));
+        var gameQuestion = new GameQuestion(0, gameId, this.question.questionId);
+        this.http.post('http://localhost:3000/gamequestions', gameQuestion).subscribe(result => {
+            window.location.reload();
+        }, error => console.log(error));
+    }
+
+    constructor(private http: Http) { }
+
+    ngOnInit() {
+    }
+}
+
