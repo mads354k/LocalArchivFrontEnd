3a47954da4c17af88857fce9f8e6f468ad38a150
Mads Peter Østergaard <momfy@hotmail.dk>
2018-05-29 08:56:48 +0200

    added a game app with shorter name

--- GameApp/GameApp.Android/Resources/Resource.designer.cs
+++ GameApp/GameApp.Android/Resources/Resource.designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("LocalArchivGameApp.Droid.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("GameApp.Droid.Resource", IsApplication=true)]
 
-namespace LocalArchivGameApp.Droid
+namespace GameApp.Droid
 {
 	
 	
@@ -26,7 +26,7 @@ namespace LocalArchivGameApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::LocalArchivGameApp.Droid.Resource.Attribute.actionBarSize;
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::GameApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
